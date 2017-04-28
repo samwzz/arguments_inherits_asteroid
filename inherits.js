f6d59bc63d4827abcbb0a2a5386773a9e91b0910
Sam Wang <sam.zz.wang@gmail.com>
2017-04-28 15:07:21 -0700

    Setup asteroid files

---
+++ inherits.js
@@ -0,0 +1,31 @@
+Function.prototype.inherits = function(parentClass) {
+  // this.inherits(parentClass)
+  function Surrogate() {
+    Surrogate.prototype = parentClass.prototype;
+    this.prototype = new Surrogate();
+    this.prototype.constructor = this;
+  }
+};
+
+Function.prototype.inherits2 = function(parentClass) {
+  this.prototype = Object.create(parentClass.prototype);
+  this.prototype.constructor = this;
+};
+
+function Person(name) {
+  this.name = name;
+}
+
+Person.prototype.eats = function(food) {
+  console.log(`${this.name} eats ${food}.`);
+};
+
+function Bro(name) {
+  Person.call(this, name);
+}
+
+Bro.inherits2(Person);
+
+const Joe = new Bro("Joe");
+
+console.log(Joe.eats("cake"));
